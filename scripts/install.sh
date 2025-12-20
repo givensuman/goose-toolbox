7fb332e4ea78e4996163bad4b4f3b11e07b28b6b
givensuman <givensuman@duck.com>
2025-12-19 20:45:16 -0500

    Fix build

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,13 +4,13 @@ if [[ $EUID -ne 0 ]]; then
   exec sudo bash -s -- "$@"
 fi
 
-if [[ command -v toolbox || -f /usr/bin/toolbox ]]; then
+if [[ $(command -v toolbox) || -f /usr/bin/toolbox ]]; then
   bin=$(command -v toolbox || /usr/bin/toolbox)
   echo "toolbox is already installed at ${bin}"
   exit 1
 fi
 
-if [[ ! command -v distrobox ]]; then
+if [[ ! $(command -v distrobox) ]]; then
   echo "distrobox is required but not found"
   exit 1
 fi
